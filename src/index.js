8c65c3f24a1cae64d32a194eed317ef73dc216c3
RacheyB94 <rlbrooks94@outlook.com>
2024-10-11 14:32:03 +0100

    Injected forecast HTML into javascript

--- src/index.js
+++ src/index.js
@@ -53,3 +53,24 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-temps">
+              <div class="weather-forecast-temp"><strong>18° </strong></div>
+              <div class="weather-forecast-temp">12°</div>
+            </div>
+          </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+displayForecast();
